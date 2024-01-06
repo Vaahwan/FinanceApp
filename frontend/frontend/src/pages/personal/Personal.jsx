98875d8c5cd70c44f58ad5953aeec1b1c008805c
Aahwan verma <vaahwan@gmail.com>
2024-01-06 23:20:52 +0530

    expense post by frontend done

--- frontend/frontend/src/pages/personal/Personal.jsx
+++ frontend/frontend/src/pages/personal/Personal.jsx
@@ -3,6 +3,7 @@ import './personal.css'
 import { useState } from "react";
 import axios from 'axios'
 import Select from 'react-select';
+import {useJwt} from 'react-jwt'
 import { Heading } from '@chakra-ui/react'
 import { Input,  InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 
@@ -30,14 +31,54 @@ const Personal = () => {
     const [date,setDate] = useState("");
     const [expense,setExpense] = useState("");
     const [expenseType,setExpenseType] = useState("");
+    const [dateErr,setDateErr] = useState(false);
+    const [expenseErr,setExpenseErr] = useState(false);
+    const [expenseTypeErr,setExpenseTypeErr] = useState(false);
     const api = "http://localhost:8080/expensetracker/expense"
+    const jwtpassword = 'vaahwan'
+    const jwtToken = localStorage.getItem('jwtToken');
+    const { decodedToken, isExpired } = useJwt(jwtToken);
 
     const handleSelect = (selectedOption)=>{
         setExpenseType(selectedOption.value);
     }
 
-    const handleSubmit = ()=>{
-        console.log('submit');
+    const handleSubmit = async()=>{
+        if(date===""){
+            setDateErr(true);
+            setTimeout(()=>{
+                setDateErr(false);
+            },3000)
+        }
+        if(expense===""){
+            setExpenseErr(true);
+            setTimeout(() => {
+                setExpenseErr(false)
+            }, 3000);
+        }
+        if(expenseType===""){
+            setExpenseTypeErr(true)
+            setTimeout(() => {
+                setExpenseTypeErr(false)
+            }, 3000);
+        }
+        const currdate = new Date();
+        const expenseObj = {
+            date : date,
+            month : currdate.getMonth(),
+            year : currdate.getFullYear(),
+            expense : Number(expense),
+            expenseType : expenseType,
+            userEmail : decodedToken.email
+        }
+        console.log(expenseObj)
+        console.log(jwtToken)
+        const response = await axios.post(api,expenseObj,{
+            headers:{
+                "Authorization": `Bearer ${jwtToken}`
+            }
+        });
+        console.log(response);
     }
 
     return (
@@ -51,7 +92,9 @@ const Personal = () => {
                     className="input"
                     onChange={(e)=>{setDate(e.target.value)}}
                 />
+                {dateErr && <p style={{ color: 'red' }}>Please Select Date</p>}
                 <Input className="input" type='number' placeholder='Enter Your Expense' size='lg' onChange={(e)=>{setExpense(e.target.value)}} />
+                {expenseErr && <p style={{ color: 'red' }}>Please Enter Expense</p>}
                 <Select
                     placeholder="Select Expense Type"
                     options={options}
@@ -60,6 +103,7 @@ const Personal = () => {
                     onChange={handleSelect}
                     size='lg'
                 />
+                {expenseTypeErr && <p style={{ color: 'red' }}>Please Select Expense Type</p>}
                 <Button bg='var(--primary-color)' color='white' size='lg' mt='4' mb='4' pr='14' pl='14' _hover={{
                     background: "white",
                     color: "var(--primary-color)",
