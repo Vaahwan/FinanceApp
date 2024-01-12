a2b289f9b4b35c8fe205685d1d40a157ffca323e
Aahwan verma <vaahwan@gmail.com>
2024-01-12 15:49:26 +0530

    nested chart added

--- frontend/frontend/src/component/NetwealthChart.jsx
+++ frontend/frontend/src/component/NetwealthChart.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import Chart from 'chart.js/auto'
 import BarChart from "./BarChart";
 import LineChart from "./LineChart";
 import Loader from "react-js-loader";
 
-const NetwealthChart = ({refresh})=>{
-    const [data,setData] = useState({});
+const NetwealthChart = ({ refresh }) => {
+    const [data, setData] = useState({});
     const url = 'http://localhost:8080/netwealthtracker/netwealth';
     const jwtToken = localStorage.getItem('jwtToken');
 
@@ -23,25 +23,40 @@ const NetwealthChart = ({refresh})=>{
         })
         const fetchedata = response.data;
         console.log(fetchedata)
-        const label = fetchedata.map((elem)=>{
+        const label = fetchedata.map((elem) => {
             return converDateFormat(elem.date);
         })
-        const dataset = fetchedata.map((elem)=>{
+        const dataset = fetchedata.map((elem) => {
             return elem.total;
+        })
+        const dataset2 = fetchedata.map((elem) => {
+            return elem.stock;
         })
         setData({
             labels: label,
             datasets: [
                 {
-                  label: "Expense",
-                  data: dataset,
-                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                  borderColor: 'rgb(75, 192, 192)',
-                  borderWidth: 2,
-                  fill: true,
-                  borderJoinStyle: 'round',
-                  tension: 0.1,
+                    label: "Net wealth",
+                    data: dataset,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 2,
+                    fill: true,
+                    borderJoinStyle: 'round',
+                    tension: 0.1,
+                    borderWidth: 3
                 },
+                {
+                    label: "stock",
+                    data: dataset2,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 2,
+                    fill: true,
+                    borderJoinStyle: 'round',
+                    tension: 0.1,
+                    borderWidth: 3
+                }
             ],
         })
     }
@@ -54,14 +69,14 @@ const NetwealthChart = ({refresh})=>{
         let formattedDate = month + '/' + year;
         return formattedDate;
     }
-    
-    return(
+
+    return (
         <div>
             {
-                data.datasets? <LineChart chartData={data} /> : <Loader type="bubble-scale" bgColor="var(--primary-color)" color="var(--secondary-color)" size={50} />
+                data.datasets ? <LineChart chartData={data} /> : <Loader type="bubble-scale" bgColor="var(--primary-color)" color="var(--secondary-color)" size={50} />
             }
         </div>
-    )   
+    )
 }
 
 export default NetwealthChart;
